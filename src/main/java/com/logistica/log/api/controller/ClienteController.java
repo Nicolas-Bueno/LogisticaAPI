e257c7f5f01169705af602f9035f3925c2236225
Nicolas Bueno <nicolasbrian202@gmail.com>
2023-01-22 12:00:03 -0300

    jakarta persistence

--- src/main/java/com/logistica/log/api/controller/ClienteController.java
+++ src/main/java/com/logistica/log/api/controller/ClienteController.java
@@ -1,6 +1,5 @@
 package com.logistica.log.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,22 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.logistica.log.domain.model.Cliente;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
 @RestController
 public class ClienteController {
+
+	@PersistenceContext
+	private EntityManager maneger;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> Listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João");
-		cliente1.setTelefone("99988-1125");
-		cliente1.setEmail("joaodascouve@gmail.com");
-
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("8985-3025");
-		cliente2.setEmail("mariasilva@gmail.com");
-		return Arrays.asList(cliente1,cliente2);
+		
+		return maneger.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }
