ce9aea1cdf1b57edb2d2aee994d47e4f7b4d8db7
Nicolas Bueno <nicolasbrian202@gmail.com>
2023-01-22 16:42:24 -0300

    Spring data jpa

--- src/main/java/com/logistica/log/api/controller/ClienteController.java
+++ src/main/java/com/logistica/log/api/controller/ClienteController.java
@@ -6,19 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.logistica.log.domain.model.Cliente;
+import com.logistica.log.domain.repository.ClienteRepository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class ClienteController {
-
-	@PersistenceContext
-	private EntityManager maneger;
+        
+        @Autowired
+        private ClienteRepository clienteRepository;
+        
+        public ClienteController (ClienteRepository clientRepository){
+            super();
+            this.clienteRepository = clientRepository;
+        }
 	
 	@GetMapping("/clientes")
 	public List<Cliente> Listar() {
-		
-		return maneger.createQuery("from Cliente", Cliente.class).getResultList();
+            return clienteRepository.findByNomeContaining("s");
 	}
 }
